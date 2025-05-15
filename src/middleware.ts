d4514f22ad3f0a8eb904faccb639db320a1fc87d
Rashmi Ranjan Parija <126317142+rashmiofficial45@users.noreply.github.com>
2025-05-15 23:34:28 +0530

    feat: add CORS support and improve middleware structure

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,13 @@
-import { NextFunction , Request , Response } from "express"
+import { NextFunction, Request, Response } from "express";
 
-export function authMiddleware (req:Request,res:Response,next:NextFunction) {
-    const token = req.headers.authorization
-    console.log("auth middleware")
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.headers.authorization;
+  if (!token) {
+    console.log("Need to login first to use this route");
+  }
+  next()
 }
--- src/router/zap.ts
+++ src/router/zap.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware";
 const router = Router();
-router.get("/",authMiddleware, (req,res) => {
-  console.log("get route in zap");
-});
+
 router.post("/",authMiddleware, (req,res) => {
-  console.log("post route in zap");
+  console.log("create a zap for a user");
 });
+
+router.get("/", authMiddleware, (req, res) => {
+  console.log("see the existing zap for a user");
+});
+
+router.get("/zapId:", authMiddleware , (req,res)=>{
+  console.log("route to see the individual zap with zapId")
+})
 export const zapRouter = router;
